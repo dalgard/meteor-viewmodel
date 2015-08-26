1b2060fda85abe22cc54619a74b8bf42d32425ce
Kristian Dalgård <k.dalgard@gmail.com>
2015-08-26 15:56:30 +0200

    Factory function in example

--- examples/verbose/client/views/bindings/throttled.js
+++ examples/verbose/client/views/bindings/throttled.js
@@ -1,3 +1,6 @@
-Template.throttled.viewmodel({
-  value: ""
+Template.throttled.viewmodel(function (template_data) {
+  // Return a definition object from this factory
+  return {
+    value: template_data && template_data.startValue || ""
+  };
 });
