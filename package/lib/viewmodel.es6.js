500944bd8c670d15aa4abd72bbabc494ea85edbb
Kristian Dalgård <k.dalgard@gmail.com>
2015-08-31 15:08:30 +0200

    Making the value property on getter-setters public

--- package/lib/viewmodel.es6.js
+++ package/lib/viewmodel.es6.js
@@ -141,7 +141,7 @@ ViewModel = class ViewModel {
             if (this.option("share")) {
               let shared = ViewModel.find(vm => vm._id === this._id);
 
-              _.each(shared, vm => vm[key]._value.set(new_value));
+              _.each(shared, vm => vm[key].value.set(new_value));
             }
           }
           else
@@ -157,7 +157,7 @@ ViewModel = class ViewModel {
 
       // Save reference to value
       if (value)
-        this[key]._value = value;
+        this[key].value = value;
 
 
       let helper = {};
