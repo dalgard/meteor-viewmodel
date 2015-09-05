df44f68ca3aff9285d5b69da28b47b8bcbf5fefc
Kristian Dalgård <k.dalgard@gmail.com>
2015-09-05 18:40:15 +0200

    More correct sequence of set and get functions in bindings (for demonstration purposes)

--- packages/dalgard_viewmodel/bindings/radio.js
+++ packages/dalgard_viewmodel/bindings/radio.js
@@ -1,12 +1,12 @@
 ViewModel.addBinding("radio", {
+  get: function (event, elem) {
+    return elem.val();
+  },
+  
   on: "change",
 
   set: function (elem, new_value) {
     if (elem.val() === new_value)
       elem.prop("checked", true);
-  },
-
-  get: function (event, elem) {
-    return elem.val();
   }
 });
