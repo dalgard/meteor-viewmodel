5f27d81dfdbb166c0a621711022fa224df23e899
Kristian Dalgård <k.dalgard@gmail.com>
2015-09-16 04:18:05 +0200

    Removed debug statements

--- packages/dalgard_viewmodel/bindings/checked.js
+++ packages/dalgard_viewmodel/bindings/checked.js
@@ -1,13 +1,11 @@
 ViewModel.addBinding("checked", {
   set: function ($elem, new_value) {
-    console.log("set");
     $elem.prop("checked", new_value);
   },
 
   on: "change",
 
   get: function (event, $elem) {
-    console.log("get");
     return $elem.prop("checked");
   }
 });
