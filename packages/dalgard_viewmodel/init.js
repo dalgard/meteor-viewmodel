332785fce20f0a3f20c300d0d6681fac539f04d3
Kristian Dalgård <k.dalgard@gmail.com>
2015-09-16 04:14:55 +0200

    Better hot code push recovery

--- packages/dalgard_viewmodel/init.js
+++ packages/dalgard_viewmodel/init.js
@@ -1,6 +1,11 @@
 // Attach declaration hook to Blaze templates
-Blaze.Template.prototype.viewmodel = ViewModel.viewmodelHook;
+Template.prototype.viewmodel = ViewModel.viewmodelHook;
 
 // Restore viewmodel values after a hot code push
-if (ViewModel.restoreAfterHCP)
-  Template.body.onRendered(ViewModel._restoreAll);
+if (ViewModel.restoreAfterHCP) {
+  // Body is created as the first event after hot code push
+  Template.body.onCreated(_.partial(ViewModel._isHCP, true));
+
+  // Body is done rendering as the last event after hot code push
+  Template.body.onRendered(_.partial(ViewModel._isHCP, false));
+}
