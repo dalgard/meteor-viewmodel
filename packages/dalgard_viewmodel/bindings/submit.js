3b5aaae6422ceac470df65f2655101f431d2ef21
Kristian Dalgård <k.dalgard@gmail.com>
2015-09-18 08:55:20 +0200

    Improved arguments for built-in bindings

--- packages/dalgard_viewmodel/bindings/submit.js
+++ packages/dalgard_viewmodel/bindings/submit.js
@@ -2,7 +2,9 @@ ViewModel.addBinding("submit", {
   on: "submit",
 
   get: function (event, $elem, prop, args, kwhash) {
-    if (args[0] !== "true")
+    var send = _.isBoolean(kwhash.send) ? kwhash.send : args[1] === "true";
+
+    if (!send)
       event.preventDefault();
 
     prop(event, args, kwhash);
