ab4b14796f89213af16d8bc6f8d255292901f8d2
Kristian Dalgård <k.dalgard@gmail.com>
2016-02-26 09:52:05 +0100

    Bump

--- packages/dalgard_viewmodel/package.js
+++ packages/dalgard_viewmodel/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "dalgard:viewmodel",
-  version: "1.0.0",
+  version: "1.0.1",
   summary: "Minimalist VM for Meteor",
   git: "https://github.com/dalgard/meteor-viewmodel",
   documentation: "../../README.md",
